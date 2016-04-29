f6b5e7cdc25a84772e9e05147207b92aa799fff8
kuramu1108 <colagarychen@gmail.com>
2016-04-29 14:38:27 +1000

    user class update
    added polls attribute

--- src/com/confer/User.java
+++ src/com/confer/User.java
@@ -1,6 +1,7 @@
 package com.confer;
 
 import java.io.Serializable;
+import java.util.*;
 
 import javax.xml.bind.annotation.*;
 
@@ -8,12 +9,14 @@ public class User implements Serializable {
 	private String email;
 	private String username;
 	private String password;
+	private ArrayList<Poll> polls;
 	
 	public User(String email, String username, String password) {
 		super();
 		this.email = email;
 		this.username = username;
 		this.password = password;
+		this.polls = new ArrayList<Poll>();
 	}
 	
 	public String getEmail() {
@@ -38,5 +41,20 @@ public class User implements Serializable {
 	
 	public void setPassword(String password) {
 		this.password = password;
+	}
+	
+	public void addPoll(Poll poll)
+	{
+		polls.add(poll);
+	}
+	
+	public void removePoll(Poll poll)
+	{
+		polls.remove(poll);
+	}
+	
+	public ArrayList<Poll> getPolls()
+	{
+		return polls;
 	}
 }
